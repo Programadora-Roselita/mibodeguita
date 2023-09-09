7170afc1382c32059a981e9f13f0cc6462bf20ef
Programadora-Roelita <rtellog@isagustin.edu.pe>
2023-09-08 20:02:57 -0500

    Cree mi proyecto con sus: capa datos. capa negocio y capa presentacion

---
+++ src/CapaNegocio/prueba_BD.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CapaNegocio;
+
+/**
+ *
+ * @author LABORATORIOA_PC10
+ */
+public class prueba_BD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
